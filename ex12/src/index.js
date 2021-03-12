cdd09cc7c57ea2564102d2c2c774b2de3acd298c
keenchy <h.kenan81@hotmail.com>
2021-03-12 22:21:31 +0100

    Using BN to Find the last Character in a String

---
+++ ex12/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+
+    // Only change code below this line
+
+    var lastName = "Tesla";
+    var lastLetterofLastName = "a";
+
+    // Only change code above this line
+
+    return lastLetterofLastName;
+}
+
+console.log(main());
+module.exports = main;
