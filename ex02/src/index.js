0f622ffc94059b150d628cdca39b5075ed11fef5
keenchy <h.kenan81@hotmail.com>
2021-03-12 19:59:10 +0100

    Quoting Strings with single Quotes

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+
+    // Only change code below this line
+    var doubleQuote = 'Jack asks Finn, "Hey, let\'s go on an adventure ? "';
+    var singleQuote = 'Finn exclaim to Jack, "Algebraic!"';
+    var myLink = "<a href=\"https: //autotelicum.github.io\" target=\"_blank\"> Link < /a > "
+        // Only change code above this line
+
+    return myLink;
+}
+
+
+console.log(main());
+module.exports = main;
