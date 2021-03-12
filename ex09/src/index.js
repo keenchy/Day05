a36af1682bbf4e8cc1f7e7b17dce57d976f94a32
keenchy <h.kenan81@hotmail.com>
2021-03-12 21:44:36 +0100

    Finding first character in a String

---
+++ ex09/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+
+    // Only change code below this line
+
+    var lastName = "Doe";
+    var firstLetterofLastName = "D";
+
+    // Only change code above this line
+
+    return firstLetterofLastName;
+}
+
+console.log(main());
+module.exports = main;
