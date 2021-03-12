954fdf9135d222ef818b6f99942d87c3fe56317b
keenchy <h.kenan81@hotmail.com>
2021-03-12 20:36:18 +0100

    Strings with Variables

---
+++ ex06/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+
+    // Only change code below this line
+
+    var myName = "Kenan";
+    var myString = "Hello, my name is " + myName + ", how are you ? ";
+
+    // Only change code above this line
+
+    return myString;
+}
+
+
+console.log(main());
+module.exports = main;
