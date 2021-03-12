ff0abc22e2d2ee79943e479930d80e2fa38f2e54
keenchy <h.kenan81@hotmail.com>
2021-03-12 23:20:19 +0100

    WordBlanks

---
+++ ex14/src/index.js
@@ -0,0 +1,20 @@
+function main() {
+
+    // Only change code below this line
+
+    var myNone = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+
+    var wordBlanks = myAdjective + myNone + myVerb + myAdverb;
+    var sentence = "that" + "big" + "dog" + "ran" + "quickly" + ".";
+
+
+    // Only change code above this line
+
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
