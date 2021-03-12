2c0b8e3d8319ac7ef77113ac4fdc4316beb7b0be
keenchy <h.kenan81@hotmail.com>
2021-03-12 20:18:53 +0100

    Strings with the Plus Equals

---
+++ ex05/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+
+    // Only change code below this line
+
+    var myString = "I come first.";
+    myString += "I come second.";
+
+    // Only change code above this line
+
+    return myString;
+}
+
+
+console.log(main());
+module.exports = main;
