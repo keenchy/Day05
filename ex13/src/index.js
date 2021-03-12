abd7f97aada5d9a51c3c17bab553269307b7dd58
keenchy <h.kenan81@hotmail.com>
2021-03-12 22:48:59 +0100

    Using .lenght to get third last letter

---
+++ ex13/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+
+    // Only change code below this line
+
+    var lastName = "Bezos";
+    var thirdToLastLetterofLastName = lastName[lastName.length - 3];
+
+    // Only change code above this line
+
+    return thirdToLastLetterofLastName;
+}
+
+console.log(main());
+module.exports = main;
