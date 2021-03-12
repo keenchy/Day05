ad1528b6be34029025fcf536fb11b3484132e3bd
keenchy <h.kenan81@hotmail.com>
2021-03-12 22:15:31 +0100

    Using Bracket Notation to Find Character in a String

---
+++ ex11/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+
+    // Only change code below this line
+
+    var lastName = "Tesla";
+    var thirdLetterofLastName = "s";
+
+    // Only change code above this line
+
+    return thirdLetterofLastName;
+}
+
+console.log(main());
+module.exports = main;
